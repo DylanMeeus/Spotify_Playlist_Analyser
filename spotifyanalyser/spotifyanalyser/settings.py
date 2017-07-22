6fcc265d804403f1f470355106ac1b382b403f11
Dylan Meeus <meeusdylan@hotmail.com>
2017-07-22 15:07:28 +0200

    Add bootstrap

--- spotifyanalyser/spotifyanalyser/settings.py
+++ spotifyanalyser/spotifyanalyser/settings.py
@@ -118,3 +118,11 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.11/howto/static-files/
 
 STATIC_URL = '/static/'
+
+STATIC_ROOT = 'analysis'
+
+
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, "static"),
+    '/analysis/static/analysis/',
+]
--- spotifyanalyser/spotifyanalyser/urls.py
+++ spotifyanalyser/spotifyanalyser/urls.py
@@ -15,8 +15,13 @@ Including another URLconf
 """
 from django.conf.urls import url,include
 from django.contrib import admin
+from django.conf import settings
+from django.conf.urls.static import static
+
+print("url appending..")
 
 urlpatterns = [
     url(r'^analysis/', include('analysis.urls')),
     url(r'^admin/', admin.site.urls),
-]
+] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+
